fffd0287517a7a85c600006ae763c540cd5a5430
ramazansakin <ramazansakin63@gmail.com>
2023-12-07 14:54:09 +0300

    feat: dummy repo up & related dummy rep def

--- src/main/java/com/example/continuesaggregatesample/dummy/DummyRepository.java
+++ src/main/java/com/example/continuesaggregatesample/dummy/DummyRepository.java
@@ -1,6 +1,15 @@
 package com.example.continuesaggregatesample.dummy;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface DummyRepository extends JpaRepository<Dummy, Integer> {
+
+    @Query(value = "SELECT * FROM Dummy d ORDER BY d.time DESC LIMIT 3", nativeQuery = true)
+    List<Dummy> findLastThreeRows();
+
 }
---
+++ src/main/java/com/example/continuesaggregatesample/dummy/RelatedDummyRepository.java
@@ -0,0 +1,9 @@
+package com.example.continuesaggregatesample.dummy;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface RelatedDummyRepository extends JpaRepository<RelatedDummy, Integer> {
+}
