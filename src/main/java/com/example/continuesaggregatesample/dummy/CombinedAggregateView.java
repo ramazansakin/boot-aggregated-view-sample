618b4383ac7be306967540d640fe130ed7ad9b34
ramazansakin <ramazansakin63@gmail.com>
2023-12-07 14:54:53 +0300

    feat: combined aggregated view usage trial

---
+++ src/main/java/com/example/continuesaggregatesample/dummy/CombinedAggregateView.java
@@ -0,0 +1,15 @@
+package com.example.continuesaggregatesample.dummy;
+
+import java.sql.Timestamp;
+
+public class CombinedAggregateView {
+
+    private Timestamp timestamp;
+    private String team;
+    private Integer totalTestOne;
+    private Double avgTestTwo;
+    private Integer totalOtherTestOne;
+    private Double avgOtherTestTwo;
+    // ... additional fields or calculations as needed
+
+}
