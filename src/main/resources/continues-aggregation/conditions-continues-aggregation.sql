1962556c38428a4662658079eaee03bd94f75e4a
ramazansakin <ramazansakin63@gmail.com>
2023-12-08 13:51:22 +0300

    feat: sample hierarchical continues aggregate sample usage  on dummy cont aggregates

--- src/main/resources/continues-aggregation/conditions-continues-aggregation.sql
+++ src/main/resources/continues-aggregation/conditions-continues-aggregation.sql
@@ -1,7 +1,7 @@
+-- Select
 SELECT * FROM public.dummy;
 
 SELECT * FROM public.related_dummy;
-
 
 SELECT * FROM public.aggregated_dummies_5mins order by bucket;
 
@@ -43,12 +43,12 @@ FROM dummy
 GROUP BY bucket, team
     WITH NO DATA;
 
-
+-- I m thinking about the set data retention period as 3 months
 SELECT add_continuous_aggregate_policy(
                'aggregated_dummies_5mins',
                start_offset => INTERVAL '30 mins',
                end_offset => INTERVAL '10 mins',
-               schedule_interval => INTERVAL '5 mins');
+               schedule_interval => INTERVAL '4 months');
 
 
 
@@ -67,7 +67,47 @@ SELECT add_continuous_aggregate_policy(
                'related_dummy_aggregate_5min',
                start_offset => INTERVAL '30 mins',
                end_offset => INTERVAL '10 mins',
-               schedule_interval => INTERVAL '5 mins');
+               schedule_interval => INTERVAL '4 months');
+
+
+CREATE MATERIALIZED VIEW aggregated_dummies_20mins
+WITH (timescaledb.continuous) AS
+SELECT
+    time_bucket('20 minutes', bucket) AS s_bucket,
+    team,
+    max(sum_testone) AS s_sum_testone,
+    min(min_testtwo) AS s_min_testtwo,
+    avg(avg_testthree) AS s_avg_testthree
+FROM aggregated_dummies_5mins
+GROUP BY s_bucket, team
+    WITH NO DATA;
+
+
+SELECT add_continuous_aggregate_policy(
+               'aggregated_dummies_20mins',
+               start_offset => INTERVAL '1 month',
+               end_offset => INTERVAL '10 mins',
+               schedule_interval => INTERVAL '4 months');
+
+
+CREATE MATERIALIZED VIEW aggregated_dummies_hourly
+WITH (timescaledb.continuous) AS
+SELECT
+    time_bucket('1 hour', s_bucket) AS h_bucket,
+    team,
+    max(s_sum_testone) AS h_sum_testone,
+    min(s_min_testtwo) AS h_min_testtwo,
+    avg(s_avg_testthree) AS h_avg_testthree
+FROM aggregated_dummies_20mins
+GROUP BY h_bucket, team
+    WITH NO DATA;
+
+
+SELECT add_continuous_aggregate_policy(
+               'aggregated_dummies_hourly',
+               start_offset => INTERVAL '1 month',
+               end_offset => INTERVAL '10 mins',
+               schedule_interval => INTERVAL '4 months');
 
 
 -- just Device-based aggregation data per 1 hour
@@ -85,8 +125,6 @@ GROUP BY device, bucket;
 
 
 SELECT add_continuous_aggregate_policy('conditions_5min_agg',
-                                        start_offset => INTERVAL '30 days',
-                                        end_offset => INTERVAL '5 days',
-                                        schedule_interval => INTERVAL '1 hour');
-
-
+                                       start_offset => INTERVAL '30 days',
+                                       end_offset => INTERVAL '5 days',
+                                       schedule_interval => INTERVAL '1 hour');
