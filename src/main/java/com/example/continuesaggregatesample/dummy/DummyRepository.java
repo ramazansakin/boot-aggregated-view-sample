03f9bc4b0b1c053713b051293b7ecddded9a637c
ramazansakin <ramazansakin63@gmail.com>
2023-12-08 07:12:29 +0300

    feat: To get combined resultset & convert to a view, related native query and conversion logic impl

--- src/main/java/com/example/continuesaggregatesample/dummy/DummyRepository.java
+++ src/main/java/com/example/continuesaggregatesample/dummy/DummyRepository.java
@@ -2,8 +2,12 @@ package com.example.continuesaggregatesample.dummy;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -11,5 +15,48 @@ public interface DummyRepository extends JpaRepository<Dummy, Integer> {
 
     @Query(value = "SELECT * FROM Dummy d ORDER BY d.time DESC LIMIT 3", nativeQuery = true)
     List<Dummy> findLastThreeRows();
+
+    @Query(value = """
+            SELECT
+                a.bucket,
+                a.team,
+                a.sum_testone AS totalTestOne,
+                a.min_testtwo AS minTestTwo,
+                r.total_othertestone AS totalOtherTestOne,
+                r.avg_othertesttwo AS avgOtherTestTwo
+            FROM aggregated_dummies_5mins AS a
+                INNER JOIN related_dummy_aggregate_5min AS r
+                    ON (a.bucket = r.bucket AND a.team = r.team)
+            WHERE a.team = :team AND a.bucket BETWEEN :start AND :end
+            """, nativeQuery = true)
+    List<Object[]> findCombinedAggregateView(
+            @Param("team") String team,
+            @Param("start") Instant start,
+            @Param("end") Instant end
+    );
+
+    default List<CombinedAggregateView> getCombinedAggregateView(String team, String start, String end) {
+        Instant startTime = Instant.parse(start);
+        Instant endTime = Instant.parse(end);
+        List<Object[]> resultList = findCombinedAggregateView(team, startTime, endTime);
+        List<CombinedAggregateView> combinedAggregateViews = new ArrayList<>();
+
+        for (Object[] result : resultList) {
+            Instant bucket = (Instant) result[0];
+            String teamName = (String) result[1];
+            Integer totalTestOne = (Integer) result[2];
+            Long minTestTwo = (Long) result[3];
+            Long totalOtherTestOne = (Long) result[4];
+            BigDecimal avgOtherTestTwo = (BigDecimal) result[5];
+
+            CombinedAggregateView view = new CombinedAggregateView(
+                    bucket, teamName, totalTestOne, minTestTwo, totalOtherTestOne, avgOtherTestTwo
+            );
+
+            combinedAggregateViews.add(view);
+        }
+
+        return combinedAggregateViews;
+    }
 
 }
