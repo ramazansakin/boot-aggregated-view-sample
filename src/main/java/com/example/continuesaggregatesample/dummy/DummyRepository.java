9a7ffdafa0b8e206a478bb6e4d8604a903af1ad4
ramazansakin <ramazansakin63@gmail.com>
2023-12-12 05:51:51 +0300

    feat: hourly & pageable queries defined

--- src/main/java/com/example/continuesaggregatesample/dummy/DummyRepository.java
+++ src/main/java/com/example/continuesaggregatesample/dummy/DummyRepository.java
@@ -1,13 +1,13 @@
 package com.example.continuesaggregatesample.dummy;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.math.BigDecimal;
 import java.time.Instant;
-import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -28,35 +28,50 @@ public interface DummyRepository extends JpaRepository<Dummy, Integer> {
                 INNER JOIN related_dummy_aggregate_5min AS r
                     ON (a.bucket = r.bucket AND a.team = r.team)
             WHERE a.team = :team AND a.bucket BETWEEN :start AND :end
+            ORDER BY bucket DESC
             """, nativeQuery = true)
-    List<Object[]> findCombinedAggregateView(
+    List<AggregatedCombineDummiesProjection> findCombinedAggregateView(
             @Param("team") String team,
             @Param("start") Instant start,
             @Param("end") Instant end
     );
 
-    default List<CombinedAggregateView> getCombinedAggregateView(String team, String start, String end) {
-        Instant startTime = Instant.parse(start);
-        Instant endTime = Instant.parse(end);
-        List<Object[]> resultList = findCombinedAggregateView(team, startTime, endTime);
-        List<CombinedAggregateView> combinedAggregateViews = new ArrayList<>();
+    @Query(value = """
+            SELECT
+                h_bucket AS bucket,
+                team,
+                h_sum_testone AS maxTestOne,
+                h_min_testtwo AS minTestTwo,
+                h_avg_testthree AS avgTestThree
+            FROM aggregated_dummies_hourly
+            WHERE team = :team AND h_bucket BETWEEN :start AND :end
+            ORDER BY bucket DESC
+            """, nativeQuery = true)
+    List<HourlyDummyAggregationProjection> findCombinedAggregateViewHourly(
+            @Param("team") String team,
+            @Param("start") Instant start,
+            @Param("end") Instant end
+    );
 
-        for (Object[] result : resultList) {
-            Instant bucket = (Instant) result[0];
-            String teamName = (String) result[1];
-            Integer totalTestOne = (Integer) result[2];
-            Long minTestTwo = (Long) result[3];
-            Long totalOtherTestOne = (Long) result[4];
-            BigDecimal avgOtherTestTwo = (BigDecimal) result[5];
-
-            CombinedAggregateView view = new CombinedAggregateView(
-                    bucket, teamName, totalTestOne, minTestTwo, totalOtherTestOne, avgOtherTestTwo
-            );
-
-            combinedAggregateViews.add(view);
-        }
-
-        return combinedAggregateViews;
-    }
+    @Query(value = """
+            SELECT
+                a.bucket,
+                a.team,
+                a.sum_testone AS totalTestOne,
+                a.min_testtwo AS minTestTwo,
+                r.total_othertestone AS totalOtherTestOne,
+                r.avg_othertesttwo AS avgOtherTestTwo
+            FROM aggregated_dummies_5mins AS a
+                INNER JOIN related_dummy_aggregate_5min AS r
+                    ON (a.bucket = r.bucket AND a.team = r.team)
+            WHERE a.team = :team AND a.bucket BETWEEN :start AND :end
+            ORDER BY bucket DESC
+            """, nativeQuery = true)
+    Page<AggregatedCombineDummiesProjection> findCombinedAggregateViewPage(
+            @Param("team") String team,
+            @Param("start") Instant start,
+            @Param("end") Instant end,
+            Pageable paging
+    );
 
 }
