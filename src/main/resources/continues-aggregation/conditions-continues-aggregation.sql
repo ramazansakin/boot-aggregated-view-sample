19146e06f4b3b2941c55504e368e240fa25fba04
ramazansakin <ramazansakin63@gmail.com>
2023-12-12 09:24:20 +0300

    feat: related epoch time based test model queries added

--- src/main/resources/continues-aggregation/conditions-continues-aggregation.sql
+++ src/main/resources/continues-aggregation/conditions-continues-aggregation.sql
@@ -131,3 +131,35 @@ SELECT add_continuous_aggregate_policy('conditions_5min_agg',
                                        start_offset => INTERVAL '30 days',
                                        end_offset => INTERVAL '5 days',
                                        schedule_interval => INTERVAL '1 hour');
+
+
+-- Timestamp as Long test model
+SELECT * FROM public.time_long_test ORDER BY time DESC;
+
+SELECT * FROM public.aggregated_long_time_5mins ORDER BY bucket DESC;
+
+DROP MATERIALIZED VIEW aggregated_long_time_5mins CASCADE;
+
+-- convert time_long_test table to hypertable
+SELECT create_hypertable('time_long_test', by_range('time', 10), migrate_data => true);
+
+CREATE FUNCTION current_epoch() RETURNS BIGINT
+    LANGUAGE SQL STABLE AS $$
+SELECT EXTRACT(EPOCH FROM CURRENT_TIMESTAMP)::bigint;$$;
+
+SELECT set_integer_now_func('time_long_test', 'current_epoch');
+
+CREATE MATERIALIZED VIEW aggregated_long_time_5mins
+WITH (timescaledb.continuous) AS
+SELECT
+    time_bucket('60', time) AS bucket,
+    group_name,
+    avg(test) AS avg_test
+FROM time_long_test
+GROUP BY bucket, group_name
+    WITH NO DATA;
+
+SELECT add_continuous_aggregate_policy('aggregated_long_time_5mins',
+                                       start_offset => 10000,
+                                       end_offset => 300,
+                                       schedule_interval => INTERVAL '5 mins');
